0554d3f0c760a05e5504b85365239dda55b8d4e5
tfm <Tom.Melham@cs.ox.ac.uk>
2025-03-18 15:56:57 +0000

    Stand-alone multiplier experiment

---
+++ dv/formal/check/multiplier.tcl
@@ -0,0 +1,15 @@
+clear -all
+
+analyze -sv12 check/multiplier.sv
+elaborate -top multiplier -disable_auto_bbox
+clock -none
+reset -none
+
+set_proofgrid_max_local_jobs 15
+set_word_level_engine_flow on
+set_engineWL_processes 15
+
+set_proof_simplification off
+
+prove -property multiplier.Mult_ALBL -engine WHp
+prove -property multiplier.Mult_ALBH -engine WHp
