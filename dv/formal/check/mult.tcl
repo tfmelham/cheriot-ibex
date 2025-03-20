df9986e1c2d18d80dde34a460c6771647d566504
tfm <Tom.Melham@cs.ox.ac.uk>
2025-03-20 14:22:44 +0000

    Mult peoperties

--- dv/formal/check/mult.tcl
+++ dv/formal/check/mult.tcl
@@ -5,9 +5,11 @@ set_engine_mode auto
 
 prove -property top.Mult_idle_ALBL
 prove -property top.Mult_ALBL_ALBH
+prove -property top.Mult_ALBH_ALBL
 prove -property top.Mult_ALBH_AHBL
 
 prove -property top.Mult_mult_en_i_stable
+prove -property top.Mult_mult_en_i_past
 prove -property top.Mult_mult_operator_i_stable
 prove -property top.Mult_op_a_stable 
 prove -property top.Mult_op_b_stable
@@ -19,15 +21,17 @@ set_word_level_engine_flow on
 set_engineWL_processes 15
 
 # 15244620 microseconds per iteration, with proven
-# 13333638 microseconds per iteration, without proven!
+# 13333638 microseconds per iteration, without proven.
 prove -property top.Mult_ALBL -engine WHp
 prove -property top.Mult_ALBL_signs -engine WHp
 
 prove -property top.Mult_ALBH_imd_val_q_i
 
-set_engine_mode {WHp WHt WI WAM K C I N}
+prove -with_proven -property top.Mult_ALBH_imd_val_q_result_extra -engine WHp
 
 stop
+
+set_engine_mode {WHp WHt WI WAM K C I N}
 
 prove -with_proven -property top.Mult_ALBH
 
